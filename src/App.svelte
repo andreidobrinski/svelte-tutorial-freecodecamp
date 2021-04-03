05e1a7ec6016f8f50ab0bda6b5f66302f54be5b6
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-04-03 17:32:20 -0400

    add routing and NavBar

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
 <script>
   import "../node_modules/materialize-css/dist/css/materialize.min.css";
   import "../node_modules/materialize-css/dist/js/materialize.min.js";
+  import { Router, Link, Route } from "svelte-routing";
+  import NavBar from "./layout/NavBar.svelte";
 </script>
 
-<main>hello world</main>
+<main>
+  <Router>
+    <NavBar />
+  </Router>
+</main>
 
 <style>
 </style>
