407854b004bd86b1b3041d79fc4defa8a3ffb5be
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-04-03 17:35:45 -0400

    add routing components

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,17 @@
   import "../node_modules/materialize-css/dist/js/materialize.min.js";
   import { Router, Link, Route } from "svelte-routing";
   import NavBar from "./layout/NavBar.svelte";
+  import Home from "./pages/Home.svelte";
+  import About from "./pages/About.svelte";
 </script>
 
 <main>
   <Router>
     <NavBar />
+    <div class="container">
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
   </Router>
 </main>
 
--- src/layout/NavBar.svelte
+++ src/layout/NavBar.svelte
@@ -4,10 +4,12 @@
 
 <nav>
   <div class="nav-wrapper">
-    <Link to="/" class="brand-logo">Svelte Tutorial</Link>
-    <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-    </ul>
+    <div class="container">
+      <Link to="/"><span class="brand-logo">Svelte Tutorial</span></Link>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </div>
   </div>
 </nav>
