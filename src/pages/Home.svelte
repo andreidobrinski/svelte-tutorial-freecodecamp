bbb4ba37defac3ed9faa87240e3815711d10a046
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-04-04 09:15:22 -0400

    add style for card action

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -20,11 +20,28 @@
         <div class="card">
           <div class="card-content">
             <p class="card-title">{posts.title}</p>
-            <p>{post.createdAt}</p>
+            <p class="timestamp">{post.createdAt}</p>
             <p>{post.body}</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Edit</a>
+            <a href="#" class="delete-btn">Delete</a>
           </div>
         </div>
       </div>
     {/each}
   {/if}
 </div>
+
+<style>
+  .delete-btn {
+    color: red !important;
+  }
+  .card .card-content .card-title {
+    margin-bottom: 0;
+  }
+  .card .card-content p.timestamp {
+    color: #999;
+    margin-bottom: 10px;
+  }
+</style>
